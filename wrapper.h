215b0de571aa0ad0b82ad4f178959c90f9fa5649
liushangzong <951597193@qq.com>
2020-01-06 09:13:10 +0800

    add wrapper.h

---
+++ wrapper.h
@@ -0,0 +1,31 @@
+#ifndef _WRAPPER_H_
+#define _WRAPPER_H_
+
+#include<stdio.h>
+#include<stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <setjmp.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <errno.h>
+#include <math.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <limits.h>
+#include <sys/types.h> 
+#include <sys/ipc.h> 
+#include <sys/msg.h>
+#include <sys/shm.h>
+#include <sys/sem.h>
+
+
